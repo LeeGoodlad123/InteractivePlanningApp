aad063df74de884675eeec3b47b4c59906cacf4c
LeeGoodlad123 <leegoodlad@hotmail.co.uk>
2025-10-19 19:42:18 +0400

    Create App component with InteractiveDigitalWallLifecycle

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import InteractiveDigitalWallLifecycle from './InteractiveDigitalWallLifecycle'
+
+export default function App() {
+  return (
+    <div className="h-screen">
+      <InteractiveDigitalWallLifecycle />
+    </div>
+  )
+}
